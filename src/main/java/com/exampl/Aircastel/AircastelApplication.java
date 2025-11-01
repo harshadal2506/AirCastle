11866d6c9ce5f50ffe67978c9beaa723be67a121
harshadal2506 <hsladole@gmail.com>
2025-11-01 14:16:41 +0530

    Add files via upload

---
+++ src/main/java/com/exampl/Aircastel/AircastelApplication.java
@@ -0,0 +1,13 @@
+package com.exampl.Aircastel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircastelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircastelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampl/Aircastel/AircastelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampl.Aircastel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircastelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
