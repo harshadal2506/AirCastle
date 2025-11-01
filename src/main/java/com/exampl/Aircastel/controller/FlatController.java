2fe6daed5cff7e5fa6501ac5757aa71ec5ea846f
harshadal2506 <hsladole@gmail.com>
2025-11-01 15:13:33 +0530

    added controller

---
+++ src/main/java/com/exampl/Aircastel/controller/FlatController.java
@@ -0,0 +1,11 @@
+package com.exampl.Aircastel.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("flats")
+public class FlatController {
+
+    
+}
